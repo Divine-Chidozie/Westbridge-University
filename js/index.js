9d327227ed1a1996dea3417b7f3228f74130297c
Divine Chidozie <igwedivine92@gmail.com>
2025-11-30 04:42:40 +0100

    added backgroun scoll to the home navbar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
+const header = document.getElementById("header");
 const hamburger = document.getElementById("hamburger");
+
+function headerScroll() {
+  const headerBackgroundColor = "#0a1f29";
+  if (window.scrollY > 50) {
+    header.style.backgroundColor = headerBackgroundColor;
+    header.style.transition = "0.3s ease-in-out";
+    document.getElementById("nav-span").style.color = "white";
+  } else if (window.scrollY < 50) {
+    header.style.backgroundColor = "";
+    document.getElementById("nav-span").style.color = "";
+  }
+}
+
+window.addEventListener("scroll", headerScroll);
 
 hamburger.addEventListener("click", () => {
   document.getElementById("menu").classList.toggle("show");
@@ -10,10 +25,10 @@ function graduate() {
 
   const graduteBtn = [underGraduateBtn, postGraduateBtn];
 
-  graduteBtn.map((btn) => {
+  graduteBtn.forEach((btn) => {
     btn.addEventListener("click", (event) => {
       event.preventDefault();
-      alert("Explore our Courses");
+      alert("Procced to choose your programme");
     });
   });
 }
